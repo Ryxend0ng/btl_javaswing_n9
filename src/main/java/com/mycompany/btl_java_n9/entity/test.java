0f7df25a1997d89030ab2f529d32fc3d2c3d0dad
ryxendong <donglon778899@gmail.com>
2022-05-16 10:58:20 +0100

    message

---
+++ src/main/java/com/mycompany/btl_java_n9/entity/test.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.btl_java_n9.entity;
+
+/**
+ *
+ * @author 0961011310
+ */
+public class test {
+    
+}
